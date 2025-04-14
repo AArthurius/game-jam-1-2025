a7ea0d0d5cccb9bf7fa789a22e80572955efcb76
AArthurius <80986328+AArthurius@users.noreply.github.com>
2025-04-14 18:06:58 -0300

    d

--- Scenes/teste/item_spawner.gd
+++ Scenes/teste/item_spawner.gd
@@ -8,6 +8,7 @@ func spawnarLenha():
 	var point = randi_range(0, spawn_area.get_children().size()-1)
 	var lenha = LENHA.instantiate()
 	lenha.global_position = spawn_area.get_child(point).global_position
+	lenha.rotation = deg_to_rad(randi_range(0, 360))
 	itens.add_child(lenha)
 
 func _on_spawn_time_timeout() -> void:
